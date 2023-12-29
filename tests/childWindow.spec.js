1c37c444b2f9f8a91dae87f6da94a88e57b5ed00
RushikeshBBankar <rushikesh1611@gmail.com>
2023-12-29 19:42:17 +0530

    manually trigger

--- tests/childWindow.spec.js
+++ tests/childWindow.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Child window handling ', async ({ browser }) => {
+test('@ui Child window handling ', async ({ browser }) => {
 
         const context = await browser.newContext();
         const page = await context.newPage();
